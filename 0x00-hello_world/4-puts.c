da3326f170df6620e3a1e451c12c5616925575c1
lawee13 <lawee13@gmail.com>
2022-12-09 13:43:34 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
+nclude <stdio.h>
+#include <stdio.h>
 #include <stdio.h>
 /*
- * main - print a line of code using the puts
- *
- * return: 0
- */
+ *  * main - print a line of code using the puts
+ *   *
+ *    * return: 0
+ *     */
  int main(void)
- {
-   put("/"Programming is like building a multilingual puzzle");
-   return (0);
+	 {
+		    put("/"Programming is like building a multilingual puzzle");
+		       return (0);
+	 }
