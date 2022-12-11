235059e691373a051e7c4b70e2bed600c9c9a1bf
lawee13 <lawee13@gmail.com>
2022-12-11 06:19:25 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,9 +11,9 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
-printf("%d is a positive\n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("%d is a negative\n", n);
+printf("%d is negative\n", n);
 else
 printf("%d is zero\n", n);
 return (0);
