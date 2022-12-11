390c1d0107126770dc482499acb16ddfe99f81b5
lawee13 <lawee13@gmail.com>
2022-12-11 13:27:48 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,24 +1,13 @@
 #include <stdio.h>
 /**
-* main - Prints all possible combinations of two different digits
-in ascending order, separated by a comma followed by a space.
+* main - Prints all alphabt in low casea
 * Return: Always 0.
 */
 int main(void)
 {
-int digit1, digit2;
-for (digit1 = 0; digit1 < 9; digit1++)
-{
-for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-{
-putchar((digit1 % 10) + '0');
-putchar((digit2 % 10) + '0');
-if (digit1 == 8 && digit2 == 9)
-continue;
-putchar(',');
-putchar(' ');
-}
-}
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
 putchar('\n');
 return (0);
 }
