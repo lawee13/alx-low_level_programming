948b7c90fe8fab86cf6b6edcc4ef2b2769a86bb3
lawee13 <lawee13@gmail.com>
2022-12-13 15:56:26 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isalpha - check if the char c is an alphabet or not
+* @c - is te random varibe elected to check
+* Return: 0 if it isnt and otherwise if it is
+*/
+int _isalpha(int c)
+{
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
