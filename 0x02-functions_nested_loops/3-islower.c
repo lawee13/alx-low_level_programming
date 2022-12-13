b155a85a80ab460243a1726133a814725d27574a
lawee13 <lawee13@gmail.com>
2022-12-13 15:13:08 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _islower- prints if the letter c is lower case or not
+ * Return 0; or 1
+ */
+int _islower(int c);
+{
+if (c >= 'a' && c <= 'z')
+return (0);
+else
+return (1);
+}
