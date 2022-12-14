22c855fbf594c9ff69fd278e8d8140e0f532b61c
lawee13 <lawee13@gmail.com>
2022-12-13 23:48:45 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - print all alphabet in lowercases 10 times
+ * print_alphabet_x10 - print all alphabet in lowercases 10 times
  */
 void print_alphabet_x10(void)
 {
