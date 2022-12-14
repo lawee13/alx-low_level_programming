2b5c01752bfd619e30f7a1f76a6ee4957a94b7c2
lawee13 <lawee13@gmail.com>
2022-12-14 06:56:38 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,18 +5,20 @@
  * Return: 0 or ortherwise 1
 */
 int print_sign(int n)
-if (n > 0)
 {
-_putchar('+');
-return (1);
-}
-else if (n == 0)
-{
-putchar('0');
-return (0);
-}
-else
-{
-_putchar('-1');
-return (-1);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-1');
+		return (-1);
+	}
 }
