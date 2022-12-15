25440ddd293bc6292fa4b43a5829a458313b1a67
lawee13 <lawee13@gmail.com>
2022-12-15 08:25:07 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include MAIN_H
+/**
+ * _abs - compute the integer value of
+ *  @n: to determine eitger its
+ *
+ *  Return: tge absolute vlue of c
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (-n);
+}
