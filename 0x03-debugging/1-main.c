687a7f1ebfbeb920b9abd8211d70398b2f777e8f
lawee13 <lawee13@gmail.com>
2022-12-15 13:01:41 -0800

    largest number task

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include 'main.h'
+/**
+ * largest_number - checks for the largest of there numbers.
+ * @a: the first interger to consider in the check
+ * @b: tge second integer to be checked
+ * @c: the third integer to be checked
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c)
+{
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	}
+		largest = c;
+	}
+	return (largest);
+}
