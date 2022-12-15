e625a3d802454de6896a6b490d5f1190d3fc0501
lawee13 <lawee13@gmail.com>
2022-12-15 10:19:48 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,9 +10,9 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-int _putchar(char c);
-void print_times_table(int n);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+int _putchar(char c);
+void print_times_table(int n);
 #endif
