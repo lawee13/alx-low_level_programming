8e6c7498cebb94b9bf855de11b4569d1a5f40a89
lawee13 <lawee13@gmail.com>
2022-12-16 13:46:19 -0800

    last call for 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,10 +13,10 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			if (j > 10)
+			if (j >= 10)
 			_putchar('i');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
+			_putchar('\n');
 	}
 }
