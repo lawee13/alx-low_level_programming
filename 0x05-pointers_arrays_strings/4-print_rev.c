6329c054f23bfe8861ca0857fe80db0109db0d0f
lawee13 <lawee13@gmail.com>
2022-12-19 16:18:19 -0800

    no 4 fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,8 +9,8 @@ void print_rev(char *s)
 
 	while (s[i] != '\0')
 		i++;
-	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i]);
+		for (i = i - 1; i >= 0; i--)
+			_putchar(s[i]);
 		
-		_putchar('\n');
+			_putchar('\n');
 }
