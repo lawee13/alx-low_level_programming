2063f07c18d384084ee0b0bc50e041099e43d17b
lawee13 <lawee13@gmail.com>
2022-12-22 15:49:13 -0800

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,19 @@
-#include "main.h"
+#include "main.h"
 /**
-  * _strcmp - compares two strings
-  * @s1: first string.
-  * @s2: second string.
-  * Return: 0 if s1 and s2 are equals,
-  * another number if not.
+ * reverse_array - reverse array of int
+ * @a: array
+ * @n: number of element of array
+ * Return: void
  */
-int _strcmp(char *s1, char *s2)
+void reverse_array(int *a, int n)
 {
-	int i = 0, op = 0;
-	        
-	while (op == 0)
+	int i;
+	int t;
+
+	for (i = 0; i < n--; i++)
 	{
-		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
-			break;
-		op = *(s1 + i) - *(s2 + i);
-		i++;
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
 	}
-	return (op);
 }
