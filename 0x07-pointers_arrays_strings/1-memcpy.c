86431edbbe54005244b1ce17a5cfbd46dab9bbbe
lawee13 <lawee13@gmail.com>
2023-01-05 04:00:45 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,7 +6,7 @@
  * @n: nun of byte
  * Return: copied memory with n byte changed
  */
-char _memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int n = 1;
