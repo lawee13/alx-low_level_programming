a46324ea8e14536a0bc8b7f35ca6914c36eacabc
lawee13 <lawee13@gmail.com>
2023-01-07 03:46:46 -0800

    folder 9 alx folder

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+void _puts(char *str)
+#include<stdio.h>
+/**
+ * main - Entry point
+ * _puts - funct puts
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
