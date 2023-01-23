a841e951c22923f755a8f35341a8aafe9e1c0d40
lawee13 <lawee13@gmail.com>
2023-01-23 14:11:17 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,12 @@
  * print_name - a function that prints a name
  * @name: the name given
  * @f: function name
- * Return: void
+ * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name == NULL || f == NULL)
+		return;
+
 		f(name)
 }
