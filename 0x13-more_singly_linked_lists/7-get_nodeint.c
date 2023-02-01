23bb540414df39a9036226faa1c8ab52ccc2a055
lawee13 <lawee13@gmail.com>
2023-02-01 12:30:19 -0800

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,6 +3,7 @@
  * get_nodeint_at_index - the nth node of a listint_T linked list
  * @head: pointer of the first index
  * @index: index of the node
+ * get_nodeint_at_index: ..
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
