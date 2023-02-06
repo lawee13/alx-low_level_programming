ff7235466c4f98683beaa406b64c5b3ac562c914
lawee13 <lawee13@gmail.com>
2023-02-06 12:28:40 -0800

    task 1 to 3

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the lenght of a string
+ * @str: pointer to the string
+ * Return: lenght of the string
+ */
+size_t _strlen(char *str)
+{
+	size_t i;
+
+	for (i = 0; str[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends a text at the end of a file
+ * @filename: name of the file
+ * @text_content: Null terminated string to add at the end of file
+ * Return: 1 on success nand -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t len;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+		len = write(fd, text_content, _strlen(text_content));
+	close(fd);
+	if (len == -1)
+		return (-1);
+	return (1);
+}
