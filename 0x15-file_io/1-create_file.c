ba645832f626f6cf87311e9c855b7e075f45055c
lawee13 <lawee13@gmail.com>
2023-02-06 13:12:13 -0800

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * create_file: createes an array of char...
+ * create_file - creates an array
  * @text_content: is a NULL terminated string to write to the file
  * @filename: is the name of the file to create
- * Return; 1 on success, -1 on failure
+ * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
